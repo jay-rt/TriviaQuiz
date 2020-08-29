ac37c221e8e06170673124b3497088f02247632d
jay-rt <38526808+jay-rt@users.noreply.github.com>
2020-08-28 22:02:20 -0400

    Implemented ViewModelFactory and repository layer

--- app/src/main/java/com/example/triviaquiz/ui/quiz/QuizActivity.java
+++ app/src/main/java/com/example/triviaquiz/ui/quiz/QuizActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.example.triviaquiz.R;
+import com.example.triviaquiz.data.QuizRepository;
 import com.example.triviaquiz.data.network.ServiceGenerator;
 import com.example.triviaquiz.data.network.model.QuizWrapper;
 import com.example.triviaquiz.databinding.ActivityQuizBinding;
@@ -27,7 +28,9 @@ public class QuizActivity extends AppCompatActivity {
         binding = ActivityQuizBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        viewModel = new ViewModelProvider(this).get(QuizViewModel.class);
+        QuizRepository repository = new QuizRepository();
+        QuizViewModelFactory factory = new QuizViewModelFactory(repository);
+        viewModel = new ViewModelProvider(this, factory).get(QuizViewModel.class);
 
         QuizAdapter adapter = new QuizAdapter(ans -> {
             Toast.makeText(QuizActivity.this, "Ans: " + ans, Toast.LENGTH_SHORT).show();
@@ -35,7 +38,6 @@ public class QuizActivity extends AppCompatActivity {
         binding.quizRecyclerView.setAdapter(adapter);
 
         viewModel.getQuizList().observe(this, adapter::submitList);
-
 
     }
 }
--- app/src/main/java/com/example/triviaquiz/ui/quiz/QuizViewModel.java
+++ app/src/main/java/com/example/triviaquiz/ui/quiz/QuizViewModel.java
@@ -4,43 +4,24 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.triviaquiz.data.QuizRepository;
 import com.example.triviaquiz.data.network.ServiceGenerator;
 import com.example.triviaquiz.data.network.model.Quiz;
 import com.example.triviaquiz.data.network.model.QuizWrapper;
 
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 public class QuizViewModel extends ViewModel {
 
-    private MutableLiveData<List<Quiz>> quizList = new MutableLiveData<>();
+    private QuizRepository repository;
+
+    public QuizViewModel(QuizRepository repository) {
+        this.repository = repository;
+        repository.loadQuizData();
+    }
 
     public LiveData<List<Quiz>> getQuizList() {
-        return quizList;
+        return repository.getQuizList();
     }
 
-    public QuizViewModel() {
-        loadQuizData();
-    }
-
-    private void loadQuizData() {
-
-        ServiceGenerator.getService().getQuizzes().enqueue(new Callback<QuizWrapper>() {
-            @Override
-            public void onResponse(Call<QuizWrapper> call, Response<QuizWrapper> response) {
-                if (response.body() != null && response.isSuccessful()) {
-                    quizList.setValue(response.body().getResults());
-//                    adapter.submitList(response.body().getResults());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<QuizWrapper> call, Throwable t) {
-
-            }
-        });
-    }
 }
